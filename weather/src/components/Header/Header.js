252f604145a7c258d83260ffb26e962a3775ec00
Javier Melendrez <81501034+javiermelendrez1@users.noreply.github.com>
2021-08-11 16:09:39 -0700

    created inputs

--- weather/src/components/Header/Header.js
+++ weather/src/components/Header/Header.js
@@ -3,6 +3,11 @@ import './Header.css'
 const Header = () => (
     <div className="header">
         <h1>React Weather App</h1>
+        <div className="inputs">
+            <label for="city">City:</label>
+            <input type="text" name="city" placeholder="Enter City"></input>
+            <button type="submit">Submit</button>
+        </div>
     </div>
 );
 export default Header;
