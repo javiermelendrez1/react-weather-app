7767dd1c582515902d237441b2fa43eb0642adad
Javier Melendrez <81501034+javiermelendrez1@users.noreply.github.com>
2021-08-11 17:15:12 -0700

    city updates state on user input

--- weather/src/App.js
+++ weather/src/App.js
@@ -5,15 +5,20 @@ import { useEffect, useState } from 'react';
 
 function App() {
   //create a state for weather and set state
-  const[weather,setWeather] = useState([]);
+  const[weather,setWeather] = useState('');
+  //create a state for city 
+  const[city,setCity] = useState('');
   //create async func to get the api
   const getApi = async () => {
     //try first
     try{
       //use axios to grab data
-      const data = await axios.get('http://api.openweathermap.org/data/2.5/weather?q=boston&appid=248f8e0c9068fd287bbbac0b4562cabf');
+      const data = await axios.get(`http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=248f8e0c9068fd287bbbac0b4562cabf`);
       //print to the console
-      console.log(data);
+      console.log(Math.ceil(data.data.main.temp));
+      //use setWeather to set the weather
+      // setWeather(Math.ceil(data.data.main.temp));
+      // setCity(city);
     } catch (e) {
       //if api fails console log error
       console.log(e);
@@ -23,10 +28,10 @@ function App() {
   useEffect(() => {
     //call function to get api
     getApi();
-  },[]);
+  },[city]); //we pass city as a dependent variable because when we will keep changing the city to see different variables
   return (
     <div className="App">
-      <Header/>
+      <Header setCity={setCity}/>
     </div>
   );
 }
--- weather/src/components/Header/Header.js
+++ weather/src/components/Header/Header.js
@@ -1,13 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Header.css'
-const Header = () => (
+const Header = ({setCity}) => {
+    //create a state for the input value
+    const[input,setInput] = useState('');
+    //this function will be called when the form is submitted
+    const handleSubmit = (e) => {
+        //prevent the page from refreshing 
+        e.preventDefault();
+        console.log(input);
+        //set the city to the input value
+        setCity(input);
+        //reset the input value
+        setInput('');
+    }
+    return (
     <div className="header">
         <h1>React Weather App</h1>
         <div className="inputs">
-            <label for="city">City:</label>
-            <input type="text" name="city" placeholder="Enter City"></input>
-            <button type="submit">Submit</button>
+            <form onSubmit={handleSubmit}>
+            <label htmlFor="city">City:</label>
+            <input type="text" name="city" placeholder="Enter City" value={input} onChange={(e) => setInput(e.target.value)}></input>
+            <button type="submit" value="submit" >Submit</button>
+            </form>
+
         </div>
     </div>
 );
+}
 export default Header;
